95a517211b24d0fbf62cc37790fa1c3bcc16cde8
Arturo Hernandez <earturohl@gmail.com>
2023-05-15 20:04:43 -0600

    tratando de renderizar categorias de la API

--- main.js
+++ main.js
@@ -8,30 +8,87 @@ function fetchAPI(url){
 fetchAPI(`${API}/categories`)
     .then(Response => Response.json())
     .then(response => {for(obj of response){
-        CAT.push(obj.name)
+        CAT.push(obj)
     }})
     .catch(error => console.log(error))
 
+fetchAPI(`${API}/products`)
+    .then(Response => Response.json())
+    .then(response => {for(obj of response){
+        PRD.push(obj)
+    }})
+    .catch(error => console.log(error))
+
+fetchAPI(`${API}/users`)
+    .then(Response => Response.json())
+    .then(response => {for(obj of response){
+        USR.push(obj)
+    }})
+    .catch(error => console.log(error))
 // OBJ Principal
 
 const CAT = []
+// {
+//     "id": 1,
+//     "name": "Clothes",
+//     "image": "https://api.lorem.space/image/fashion?w=640&h=480&r=4278"
+// }
 console.log(CAT)
+console.log(`${CAT} Categories`)
+const PRD = []
+// {
+//     "id": 4,
+//     "title": "Handmade Fresh Table",
+//     "price": 687,
+//     "description": "Andy shoes are designed to keeping in...",
+//     "category": {
+//       "id": 5,
+//       "name": "Others",
+//       "image": "https://placeimg.com/640/480/any?r=0.591926261873231"
+//     },
+//     "images": [
+//       "https://placeimg.com/640/480/any?r=0.9178516507833767",
+//       "https://placeimg.com/640/480/any?r=0.9300320592588625",
+//       "https://placeimg.com/640/480/any?r=0.8807778235430017"
+//     ]
+// }
+console.log(PRD)
+console.log(`${PRD} Products`)
+const USR = []
+// {
+//     "id": 1,
+//     "email": "john@mail.com",
+//     "password": "changeme",
+//     "name": "Jhon",
+//     "role": "customer",
+//     "avatar": "https://api.lorem.space/image/face?w=640&h=480&r=867",
+// },
+console.log(USR)
+console.log(`${USR} Users`)
+
 
 //Header
 const header = document.querySelector('.header')
 
-
 const logo = document.createElement('h1')
     logo.innerText = 'NOT'
 const navbar = document.createElement('div')
-const categories = document.createElement('div')
-    categories.innerText = 'Categories'
 const logIN = document.createElement('div')
     logIN.innerText = 'Log In'
 
-
-
-navbar.appendChild(categories)
-navbar.appendChild(logIN)
 header.appendChild(logo)
-header.appendChild(navbar)
+header.appendChild(logIN)
+
+// NAV CAT
+const navCAT = document.querySelector('.categories')
+
+// const newCategory = async()=>{
+
+//     const  = await 
+//         const newCategory = document.createElement('div')
+//             newCategory.innerText = obj.name
+//         navCAT.appendChild(newCategory)
+//         console.log('funcion llamada')
+// }
+
+// La funcion no funciona porque deberia de ser async debido a que el servidor no responde inmediatamente.
